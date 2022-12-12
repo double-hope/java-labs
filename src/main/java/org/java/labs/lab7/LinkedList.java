f0aff83e1a02d6431d530f97c36b913994959116
double-hope <nadia.prohorchuk@gmail.com>
2022-12-12 15:04:59 +0200

    create CustomIterator

--- src/main/java/org/java/labs/lab7/LinkedList.java
+++ src/main/java/org/java/labs/lab7/LinkedList.java
@@ -33,8 +33,8 @@ public class LinkedList<T> implements List<T> {
     }
 
     @Override
-    public Iterator<T> iterator() {
-        return null;
+    public CustomIterator<T> iterator() {
+        return new CustomIterator<>(new Node<>(null, this.node));
     }
 
     @Override
