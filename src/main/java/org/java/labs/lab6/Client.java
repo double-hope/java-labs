920b21c99e80ff2e6703ccc11f1e13c70bdb8735
double-hope <nadia.prohorchuk@gmail.com>
2022-12-12 14:34:11 +0200

    add Client record, getters and setters to Tariff class

---
+++ src/main/java/org/java/labs/lab6/Client.java
@@ -0,0 +1,4 @@
+package org.java.labs.lab6;
+
+public record Client(String name) {
+}
--- src/main/java/org/java/labs/lab6/Tariff.java
+++ src/main/java/org/java/labs/lab6/Tariff.java
@@ -1,16 +1,42 @@
 package org.java.labs.lab6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tariff {
     private final int cost;
     private final String name;
     private final int gigabytes;
     private final int minutes;
     private final int messages;
+    private final List<Client> clients = new ArrayList<>();
     public Tariff(int cost, String name, int gigabytes, int minutes, int messages){
         this.cost = cost;
         this.name = name;
         this.gigabytes = gigabytes;
         this.minutes = minutes;
         this.messages = messages;
+    }
+    public int getCost() {
+        return cost;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getGigabytes() {
+        return gigabytes;
+    }
+    public int getMinutes() {
+        return minutes;
+    }
+    public int getMessages() {
+        return messages;
+    }
+    public List<Client> getClients() {
+        return clients;
+    }
+    public void addClient(Client client){
+        this.clients.add(client);
     }
 }
